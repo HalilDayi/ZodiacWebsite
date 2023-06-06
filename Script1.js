ac696c03cdba2e27fc0033b6db0fa955951573e6
HalilDayi <120185611+HalilDayi@users.noreply.github.com>
2023-06-06 23:35:51 +0300

    Add files via upload

--- Script1.js
+++ Script1.js
@@ -1,8 +1,4 @@
-let zodiacs = ["CAPRICORN", "AQUARIUS", "PISCES", "ARIES", "TAURUS", "GEMINI", "CANCER", "LEO", "VIRGO", "LIBRA"
-    , "SCORPIO", "SAGITTARIUS"];
-
 function changeBackGround(element) {
-    element.classList.add("highlighted");
-    var elemId = element.id;
-    
+    element.classList.toggle("highlighted");
 }
+
